efd4a0d224b89e02cb7fbc03e7e404f6cb01b33f
Rostyslav Gress <nexusskv@i.ua>
2020-01-30 16:57:07 +0200

    - Created the project architecture. - Created List and Wish modules.

---
+++ MyWishesRating/MyWishesRating/Modules/List/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  MyWishesRating
+//
+//  Created by Rost on 30.01.2020.
+//  Copyright © 2020 Rost Gress. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ MyWishesRating/MyWishesRating/Modules/Wish/WishViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WishViewController.swift
+//  MyWishesRating
+//
+//  Created by Rost on 30.01.2020.
+//  Copyright © 2020 Rost Gress. All rights reserved.
+//
+
+import Foundation
