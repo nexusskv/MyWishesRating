6e261229840ef999bc46a8235d5da169dc6984d9
Rostyslav Gress <nexusskv@i.ua>
2020-02-01 21:11:04 +0200

    - Added to Assets an images for RatingView. - Added to Assets an images for up and down rating. - Added to Assets an images for sortItem and randomItem. - Added to Assets an images for select/deselect button as checkmark.

---
+++ MyWishesRating/MyWishesRating/Modules/Wish/Common/Managers/WishObserversManager.swift
@@ -0,0 +1,51 @@
+//
+//  WishObserversManager.swift
+//  MyWishesRating
+//
+//  Created by Rost on 01.02.2020.
+//  Copyright © 2020 Rost Gress. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class WishObserversManager {
+    
+    
+    /// ---> Function for add observers  <--- ///
+    func addObservers(_ view: WishViewController) {
+        let defaultCenter = NotificationCenter.default
+        
+        defaultCenter.addObserver(view,
+                                  selector: #selector(view.changeRating),
+                                  name: changeRatingName,
+                                  object: nil)
+        
+        defaultCenter.addObserver(view,
+                                  selector: #selector(view.changeRatingLimit),
+                                  name: changeRatingLimitName,
+                                  object: nil)
+        
+        defaultCenter.addObserver(view,
+                                  selector: #selector(view.refreshTable),
+                                  name: tableNeedRefreshName,
+                                  object: nil)
+        
+        defaultCenter.addObserver(view,
+                                  selector: #selector(view.sortViewStateNeedChange),
+                                  name: sortItemTappedName,
+                                  object: nil)
+        
+        defaultCenter.addObserver(view,
+                                  selector: #selector(view.sortTypeSelected),
+                                  name: sortTypeSelectedName,
+                                  object: nil)
+    }
+    
+    
+    /// ---> Function for remove observers <--- ///
+    func removeObservers(_ view: WishViewController) {
+        NotificationCenter.default.removeObserver(view)
+    }
+}
